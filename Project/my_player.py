6cbeb1eb24149a9d7272a85484d560667df28137
Yanis Toubal <yanis.toubal@polymtl.ca>
2021-10-11 19:33:31 -0400

    add project files

---
+++ Project/my_player.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Quoridor agent.
+Copyright (C) 2013, <<<<<<<<<<< YOUR NAMES HERE >>>>>>>>>>>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+from quoridor import *
+
+
+class MyAgent(Agent):
+
+    """My Quoridor agent."""
+
+    def play(self, percepts, player, step, time_left):
+        """
+        This function is used to play a move according
+        to the percepts, player and time left provided as input.
+        It must return an action representing the move the player
+        will perform.
+        :param percepts: dictionary representing the current board
+            in a form that can be fed to `dict_to_board()` in quoridor.py.
+        :param player: the player to control in this step (0 or 1)
+        :param step: the current step number, starting from 1
+        :param time_left: a float giving the number of seconds left from the time
+            credit. If the game is not time-limited, time_left is None.
+        :return: an action
+          eg: ('P', 5, 2) to move your pawn to cell (5,2)
+          eg: ('WH', 5, 2) to put a horizontal wall on corridor (5,2)
+          for more details, see `Board.get_actions()` in quoridor.py
+        """
+        print("percept:", percepts)
+        print("player:", player)
+        print("step:", step)
+        print("time left:", time_left if time_left else '+inf')
+
+        # TODO: implement your agent and return an action for the current step.
+        pass
+
+
+if __name__ == "__main__":
+    agent_main(MyAgent())
