840ec1979b47780e2e7838bdf1f28c6e93129771
Yanis Toubal <yanis.toubal@polymtl.ca>
2021-11-19 14:03:42 -0500

    player v2 done

--- Project/utils.py
+++ Project/utils.py
@@ -2,6 +2,9 @@
 
 import heapq
 
+def manhattan(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
 class PriorityQueue:
     """
       Implements a priority queue data structure. Each inserted item
